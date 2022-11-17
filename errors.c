e0a8d554a716d96a7b0ac4b704e3b156eb0b14c7
samuel-kinyua <samuelkinyua722@gmail.com>
2022-11-17 07:38:34 -0800

    made some indentation changes on the errors.c file

--- errors.c
+++ errors.c
@@ -1,3 +1,4 @@
+
 #include "shell.h"
 
 int num_len(int num);
@@ -71,6 +72,7 @@ char *_itoa(int num)
 	return (buffer);
 }
 
+
 /**
  * create_error - Writes to stderr.
  * @args: An array of arguments.
@@ -84,28 +86,29 @@ int create_error(char **args, int err)
 
 	switch (err)
 	{
-		case -1:
-			error = error_env(args);
-			break;
-		case 1:
-			error = error_1(args);
-			break;
-		case 2:
-			if (*(args[0]) == 'e')
-				error = error_2_exit(++args);
-			else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
-				error = error_2_syntax(args);
-			else
-				error = error_2_cd(args);
-			break;
-		case 126;
+	case -1:
+		error = error_env(args);
+		break;
+	case 1:
+		error = error_1(args);
+		break;
+	case 2:
+		if (*(args[0]) == 'e')
+			error = error_2_exit(++args);
+		else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
+			error = error_2_syntax(args);
+		else
+			error = error_2_cd(args);
+		break;
+	case 126:
 		error = error_126(args);
-		case 127:
+		break;
+	case 127:
 		error = error_127(args);
 		break;
 	}
-
 	write(STDERR_FILENO, error, _strlen(error));
+
 	if (error)
 		free(error);
 	return (err);
