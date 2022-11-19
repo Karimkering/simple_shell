545af4b7b624a96ce9b07ec832b59dc4d10dd0d6
samuel-kinyua <samuelkinyua722@gmail.com>
2022-11-18 16:41:07 -0800

    edited the helper.c file due to betty errors

--- helper.c
+++ helper.c
@@ -14,6 +14,7 @@ void variable_replacement(char **args, int *exe_ret);
 void free_args(char **args, char **front)
 {
 	size_t i;
+
 	for (i = 0; args[i] || args[i + 1]; i++)
 		free(args[i]);
 	free(front);
@@ -33,6 +34,7 @@ char *get_pid(void)
 	size_t i = 0;
 	char *buffer;
 	ssize_t file;
+
 	file = open("/proc/self/stat", O_RDONLY);
 	if (file == -1)
 	{
@@ -67,13 +69,16 @@ char *get_env_value(char *beginning, int len)
 {
 	char **var_addr;
 	char *replacement = NULL, *temp, *var;
+
 	var = malloc(len + 1);
+
 	if (!var)
 		return (NULL);
 	var[0] = '\0';
 	_strncat(var, beginning, len);
 	var_addr = _getenv(var);
 	free(var);
+
 	if (var_addr)
 	{
 		temp = *var_addr;
@@ -100,11 +105,13 @@ void variable_replacement(char **line, int *exe_ret)
 {
 	int j, k = 0, len;
 	char *replacement = NULL, *old_line = NULL, *new_line;
+
 	old_line = *line;
+
 	for (j = 0; old_line[j]; j++)
 	{
 		if (old_line[j] == '$' && old_line[j + 1] &&
-				old_line[j + 1] != ' ')								
+				old_line[j + 1] != ' ')
 		{
 			if (old_line[j + 1] == '$')
 			{
@@ -116,15 +123,17 @@ void variable_replacement(char **line, int *exe_ret)
 				replacement = _itoa(*exe_ret);
 				k = j + 2;
 			}
-			else if (old_line[j + 1])
+			else
+				(old_line[j + 1])
+			{
 				/* extract the variable name to search for */
 				for (k = j + 1; old_line[k] &&
 						old_line[k] != '$' &&
 						old_line[k] != ' '; k++)
 					;
-					len = k - (j + 1);
-					replacement = get_env_value(&old_line[j + 1], len);
-
+				len = k - (j + 1);
+				replacement = get_env_value(&old_line[j + 1], len);
+			}
 			new_line = malloc(j + _strlen(replacement)
 					+ _strlen(&old_line[k]) + 1);
 			if (!line)
